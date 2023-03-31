8ba97142c3da7b9b3099ff9a646fe4f6baad45a8
Rodrigo Bueno <rodrigo.bueno-sys@parceirosedu.com.br>
2023-03-31 16:44:16 -0300

    Adicionando validações no contrato enviado para cadastrar cartão e realizar transação.

--- cartoes-vr/src/main/java/autorizador/cartoesvr/dto/CartaoDto.java
+++ cartoes-vr/src/main/java/autorizador/cartoesvr/dto/CartaoDto.java
@@ -2,11 +2,17 @@ package autorizador.cartoesvr.dto;
 
 
 import lombok.Data;
-
-import java.math.BigDecimal;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Data
 public class CartaoDto {
+
+    @NotBlank(message = "Informe o número do cartão a ser cadastrado")
+    @Size(min = 16, max = 16, message = "O cartão deve possuir somente 16 números")
     private String numeroCartao;
+
+    @NotBlank(message = "Informe a senha")
+    @Size(min = 6, max = 6, message = "A senha deve possuir exatamente 6 digitos.")
     private String senha;
 }
--- cartoes-vr/src/main/java/autorizador/cartoesvr/dto/CartaoTransacaoDto.java
+++ cartoes-vr/src/main/java/autorizador/cartoesvr/dto/CartaoTransacaoDto.java
@@ -3,11 +3,22 @@ package autorizador.cartoesvr.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.math.BigDecimal;
 
 @Data
 public class CartaoTransacaoDto {
+
+    @NotBlank(message = "Informe o número do cartão para a transacao")
+    @Size(min = 16, max = 16, message = "O cartão deve possuir somente 16 números")
     private String numeroCartao;
+
+    @NotBlank(message = "Informe a senha")
+    @Size(min = 6, max = 6, message = "A senha deve possuir exatamente 6 digitos.")
     private String senhaCartao;
+
+    @DecimalMin(value = "0.00", inclusive = false, message = "O valor de transação deve ser maior ou igual a 0.01")
     private BigDecimal valor;
 }
